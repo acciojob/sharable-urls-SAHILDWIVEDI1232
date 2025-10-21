6d595204a97e390ee08bdc1bfcffa6ddc796854e
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-10-21 11:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+document.getElementById("myForm").addEventListener("submit",function(e){
+	e.preventDefault();
+	 const name = document.getElementById('name').value;
+            const year = document.getElementById('year').value;
+		const newUrl=`https://localhost:8080/? name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+document.getElementById('url').innerText = newUrl;});
