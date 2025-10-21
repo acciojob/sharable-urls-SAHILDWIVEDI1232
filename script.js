d8d5218be4896e357c003f8c864101ba172282f7
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-10-21 11:28:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ document.getElementById("myForm").addEventListener("submit",function(e){
 	e.preventDefault();
 	 const name = document.getElementById('name').value;
             const year = document.getElementById('year').value;
-		const newUrl=`https://localhost:8080/? name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+		const newUrl=`https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
 document.getElementById('url').innerText = newUrl;});
